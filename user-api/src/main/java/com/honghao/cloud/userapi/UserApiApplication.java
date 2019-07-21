75f0189437bf0edd7cab1c2c32dac44ca156364b
chenhonghao <949206605@qq.com>
2019-07-21 22:42:11 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/UserApiApplication.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/UserApiApplication.java
@@ -3,7 +3,10 @@ package com.honghao.cloud.userapi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,6 +14,9 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * @author chenhonghao
  * @date 2019-7-17
  */
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableFeignClients
 @EnableTransactionManagement
 @EnableEurekaClient
 @EnableSwagger2
--- user-api/src/main/java/com/honghao/cloud/userapi/controller/UserController.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/controller/UserController.java
@@ -1,13 +1,8 @@
 package com.honghao.cloud.userapi.controller;
 
-import com.alibaba.fastjson.JSONObject;
 import com.honghao.cloud.userapi.aspect.Auth;
 import com.honghao.cloud.userapi.base.BaseResponse;
 import com.honghao.cloud.userapi.facade.WaybillBcListFacade;
-import com.honghao.cloud.userapi.interceptor.UserInfoHolder;
-import com.honghao.cloud.userapi.listener.rabbitmq.producer.MessageSender;
-import com.honghao.cloud.userapi.task.AsyncTask;
-import com.honghao.cloud.userapi.utils.JedisOperator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -29,13 +24,6 @@ import javax.annotation.Resource;
 @RequestMapping("/user")
 @Api("用户接口服务")
 public class UserController {
-    private static final String DICTIONARY="dictionart_";
-    @Resource
-    private MessageSender messageSender;
-    @Resource
-    private AsyncTask asyncTask;
-    @Resource
-    private JedisOperator jedisOperator;
     @Resource
     private WaybillBcListFacade waybillBcListFacade;
 
@@ -43,17 +31,7 @@ public class UserController {
     @PostMapping("/create")
     @ApiOperation(value = "创建用户",notes = "创建用户")
     BaseResponse<Boolean> createUser(@RequestBody String data) {
-        waybillBcListFacade.createUser();
-        JSONObject jsonObject1=JSONObject.parseObject(data);
-        String batchId=jsonObject1.getString("batchId");
-        log.info("start create user ,requestParam:{}",data);
-        String agentNo = UserInfoHolder.getOperator().getAgentNo();
-        messageSender.pushInfoUser("userinfo");
-        JSONObject jsonObject=new JSONObject();
-        jsonObject.put("value","chenwenliang");
-        messageSender.delayDoAction(jsonObject);
-        jedisOperator.set(DICTIONARY+batchId,batchId);
-        asyncTask.sendInfo();
+        waybillBcListFacade.createUser(data);
         return BaseResponse.success();
     }
 
