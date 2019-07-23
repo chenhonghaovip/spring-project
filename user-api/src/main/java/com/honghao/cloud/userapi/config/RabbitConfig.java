843a404d859aa84c83c986ed8414b0b85dedc0f0
CHH <949206605@qq.com>
2019-07-23 15:49:37 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/config/RabbitConfig.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/config/RabbitConfig.java
@@ -2,10 +2,12 @@ package com.honghao.cloud.userapi.config;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -38,10 +40,26 @@ public class RabbitConfig {
     public static final String DELAY_TEN_MIN_DEATH = "delay_ten_min_death";
 
     /**
-     * 10分钟复活
+     * 10分钟复活消费队列
      */
     public static final String DELAY_TEN_MIN = "delay_ten_min";
 
+    /**
+     * 支付系统的mq交换机
+     */
+    public static final String PAY_DIRECT_EXCHANGE = "pay_direct_exchange";
+    /**
+     * 推送订单系统两分钟延迟队列
+     */
+    public static final String PUSH_TO_ORDER_QUEUE = "push_to_order_queue";
+    /**
+     * 推送订单系统重试队列
+     */
+    public static final String PUSH_TO_ORDER_QUEUE_RETRY = "push_to_order_queue_retry";
+    /**
+     * 推送订单系统失败队列
+     */
+    public static final String PUSH_TO_ORDER_QUEUE_FAIL = "push_to_order_queue_fail";
     /**
      * 默认的线程数
      */
@@ -87,6 +105,20 @@ public class RabbitConfig {
         return factory;
     }
 
+    @Bean(name = "rabbitAdmin")
+    @Primary
+    public RabbitAdmin rabbitAdmin(@Qualifier("connectionFactory") ConnectionFactory connectionFactory) {
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public DirectExchange directExchange(@Qualifier("rabbitAdmin") RabbitAdmin rabbitAdmin) {
+        DirectExchange directExchange = new DirectExchange(PAY_DIRECT_EXCHANGE);
+//        directExchange.setAdminsThatShouldDeclare(rabbitAdmin);
+        return directExchange;
+    }
+
+
     /**
      * 用户信息推送队列
      * @return queue
@@ -115,5 +147,38 @@ public class RabbitConfig {
     @Bean
     public Queue delayConsumeQueue(){
         return new Queue(DELAY_TEN_MIN);
+    }
+
+    /**
+     * 正常消费队列
+     * @return queue
+     */
+    @Bean
+    public Queue pushOrderQueue(){
+        return new Queue(PUSH_TO_ORDER_QUEUE);
+    }
+    /**
+     * 消费失败后转入延迟重试队列
+     * @return queue
+     */
+    @Bean
+    public Queue pushOrderQueueRetry(){
+        Map<String, Object> map = new HashMap<>(8);
+        //失败后重新将消息路由到具体exchange上
+//        map.put("x-dead-letter-exchange", PAY_DIRECT_EXCHANGE);
+        map.put("x-dead-letter-exchange", "");
+        //失败后重新将消息路由到具体routeKey上
+        map.put("x-dead-letter-routing-key", PUSH_TO_ORDER_QUEUE);
+        //消息延迟时间
+        map.put("x-message-ttl", 5000);
+        return new Queue(PUSH_TO_ORDER_QUEUE_RETRY);
+    }
+    /**
+     * 三次失败后转入失败队列
+     * @return queue
+     */
+    @Bean
+    public Queue pushOrderQueueFail(){
+        return new Queue(PUSH_TO_ORDER_QUEUE_FAIL);
     }
 }
