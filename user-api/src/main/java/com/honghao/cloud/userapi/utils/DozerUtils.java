f512a7a5eaf37c7c99c7df328d2d0e6a220df736
CHH <949206605@qq.com>
2019-07-30 17:47:58 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/utils/DozerUtils.java
@@ -0,0 +1,13 @@
+package com.honghao.cloud.userapi.utils;
+
+import org.dozer.DozerBeanMapper;
+
+/**
+ * 对象属性复制工具类
+ *
+ * @author chenhonghao
+ * @date 2019-07-30 17:31
+ */
+public class DozerUtils {
+    public static final DozerBeanMapper dozerBeanMapper=new DozerBeanMapper();
+}
