09d1aeecb1685435f3d5b72edefbaf6779f343a8
CHH <949206605@qq.com>
2019-08-09 15:12:17 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/test/Test.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/Test.java
@@ -9,19 +9,24 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class Test {
     public static void main(String[] args) {
-        //数字字符串
-        String name="1048576.1024";
-        //构造以字符串内容为值的BigDecimal类型的变量bd
-        BigDecimal bd=new BigDecimal(name);
-        //设置小数位数，第一个变量是小数位数，第二个变量是取舍方法(四舍五入)
-        bd=bd.setScale(2, BigDecimal.ROUND_HALF_UP);
-        //转化为字符串输出
-        System.out.println(bd);
-        String queues="[{\"queue\":\"user_push_queue\"},{\"queue\":\"delay_ten_min_death\"}]";
-        JSONArray jsonArray= JSON.parseArray(queues);
-        List<JSONObject> list = jsonArray.toJavaList(JSONObject.class);
-        List<String> values = list.stream().map(each->each.getString("queue")).collect(Collectors.toList());
-        log.info(String.valueOf(values));
+//        //数字字符串
+//        String name="1048576.1024";
+//        //构造以字符串内容为值的BigDecimal类型的变量bd
+//        BigDecimal bd=new BigDecimal(name);
+//        //设置小数位数，第一个变量是小数位数，第二个变量是取舍方法(四舍五入)
+//        bd=bd.setScale(2, BigDecimal.ROUND_HALF_UP);
+//        //转化为字符串输出
+//        System.out.println(bd);
+//        String queues="[{\"queue\":\"user_push_queue\"},{\"queue\":\"delay_ten_min_death\"}]";
+//        JSONArray jsonArray= JSON.parseArray(queues);
+//        List<JSONObject> list = jsonArray.toJavaList(JSONObject.class);
+//        List<String> values = list.stream().map(each->each.getString("queue")).collect(Collectors.toList());
+//        log.info(String.valueOf(values));
+
+        int n=50;
+        Test test = new Test();
+        int sum = test.getStep(4);
+        log.info("方法有：{}",sum);
 
     }
     private int getStep(int k){
