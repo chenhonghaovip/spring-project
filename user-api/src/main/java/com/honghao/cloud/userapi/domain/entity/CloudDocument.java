b2d3c709e88a1e9c6fbd138ff6384ba9d00112be
CHH <949206605@qq.com>
2019-08-12 10:23:32 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/domain/entity/CloudDocument.java
@@ -0,0 +1,75 @@
+package com.honghao.cloud.userapi.domain.entity;
+
+import java.util.Date;
+
+public class CloudDocument {
+    private Integer id;
+
+    private String documentName;
+
+    private String documentType;
+
+    private String documentAddress;
+
+    private Date createTime;
+
+    private String createBy;
+
+    private Byte deleteFlag;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDocumentName() {
+        return documentName;
+    }
+
+    public void setDocumentName(String documentName) {
+        this.documentName = documentName == null ? null : documentName.trim();
+    }
+
+    public String getDocumentType() {
+        return documentType;
+    }
+
+    public void setDocumentType(String documentType) {
+        this.documentType = documentType == null ? null : documentType.trim();
+    }
+
+    public String getDocumentAddress() {
+        return documentAddress;
+    }
+
+    public void setDocumentAddress(String documentAddress) {
+        this.documentAddress = documentAddress == null ? null : documentAddress.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public String getCreateBy() {
+        return createBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        this.createBy = createBy == null ? null : createBy.trim();
+    }
+
+    public Byte getDeleteFlag() {
+        return deleteFlag;
+    }
+
+    public void setDeleteFlag(Byte deleteFlag) {
+        this.deleteFlag = deleteFlag;
+    }
+}
