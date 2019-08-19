9d626c3fe48511e946c83453cc53deaab0837e0b
CHH <949206605@qq.com>
2019-08-19 16:54:45 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/controller/UserController.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/controller/UserController.java
@@ -1,14 +1,12 @@
 package com.honghao.cloud.userapi.controller;
 
+import com.honghao.cloud.userapi.aspect.Auth;
 import com.honghao.cloud.userapi.base.BaseResponse;
 import com.honghao.cloud.userapi.facade.WaybillBcListFacade;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
@@ -45,10 +43,24 @@ public class UserController {
      * @param data
      * @return
      */
+    @Auth
     @PostMapping("/create1")
     @ApiOperation(value = "测试" ,notes = "测试")
     BaseResponse<String> getUser(@RequestBody String data) {
 //        waybillBcListFacade.createUser1(data);
+        waybillBcListFacade.createUser2("");
+        return null;
+    }
+
+    /**
+     * 创建用户
+     * @param kappId 骑士id
+     * @return BaseResponse
+     */
+    @Auth
+    @GetMapping("/create2")
+    @ApiOperation(value = "测试" ,notes = "测试")
+    BaseResponse<String> create2(@RequestParam("kappId") String kappId) {
         waybillBcListFacade.createUser2("");
         return null;
     }
