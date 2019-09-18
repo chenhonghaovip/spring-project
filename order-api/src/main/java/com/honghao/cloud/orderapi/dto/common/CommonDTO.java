156f112e0a3b85d3f30893569556873c5b14cecf
CHH <949206605@qq.com>
2019-09-18 15:46:21 +0800

    commit by chh

---
+++ order-api/src/main/java/com/honghao/cloud/orderapi/dto/common/CommonDTO.java
@@ -0,0 +1,15 @@
+package com.honghao.cloud.orderapi.dto.common;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author chenhonghao
+ * @date 2019-09-18 14:45
+ */
+@Data
+@AllArgsConstructor
+public class CommonDTO {
+    private Integer code;
+    private String desc;
+}
