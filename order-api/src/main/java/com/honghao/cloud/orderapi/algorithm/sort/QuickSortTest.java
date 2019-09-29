cfcee1a8c3f49202745f7deddd21e194088753cb
CHH <949206605@qq.com>
2019-09-29 17:33:23 +0800

    commit by chh

---
+++ order-api/src/main/java/com/honghao/cloud/orderapi/algorithm/sort/QuickSortTest.java
@@ -0,0 +1,33 @@
+package com.honghao.cloud.orderapi.algorithm.sort;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * 快速排序算法
+ *
+ * @author chenhonghao
+ * @date 2019-09-23 15:03
+ */
+@Slf4j
+public class QuickSortTest {
+    public static void main(String[] args) {
+        int[] arr = new int[]{45,1233,5,34,40,2,12,232};
+        int min ;
+        int tempValue;
+        for (int i = 0; i < arr.length-1; i++) {
+            min = i;
+            for (int j = i+1; j < arr.length; j++) {
+                if (arr[j] < arr[min]){
+                    min = j;
+                }
+            }
+            if (min != i){
+                tempValue = arr[i];
+                arr[i] =  arr[min];
+                arr[min] = tempValue;
+            }
+            log.info("第{}次排序之后的数组为{}",i,arr);
+        }
+        log.info("{}",arr);
+    }
+}
---
+++ order-api/src/main/java/com/honghao/cloud/orderapi/test/GeoHashTest.java
@@ -0,0 +1,52 @@
+package com.honghao.cloud.orderapi.test;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+
+import java.math.BigDecimal;
+
+/**
+ * GEOHash测试
+ *
+ * @author chenhonghao
+ * @date 2019-09-29 16:56
+ */
+@Slf4j
+public class GeoHashTest {
+    public static void main(String[] args) {
+        BigDecimal low = BigDecimal.valueOf(-90);
+        BigDecimal high = BigDecimal.valueOf(90);
+        GeoHashTest geoHashTest = new GeoHashTest();
+        String code = geoHashTest.getLaCode(BigDecimal.valueOf(39.92324),low,high,"");
+        log.info(code);
+
+    }
+
+    /**
+     * 获取纬度编码
+     * @param value 值
+     * @param low 值
+     * @param high 值
+     * @param code 纬度编码
+     * @return String
+     */
+    public String getLaCode(BigDecimal value,BigDecimal low,BigDecimal high ,String code){
+        log.info("code为：{}",code);
+        if (StringUtils.isNotBlank(code) && code.length() > 20){
+            return code;
+        }
+        BigDecimal middle = BigDecimal.ZERO;
+        if (BigDecimal.ZERO.compareTo(low.multiply(high)) <= 0){
+            middle = high.subtract(low).divide(BigDecimal.valueOf(2));
+        }
+        if (value.compareTo(middle) > 0){
+            low = middle;
+            code = code + "1";
+        }else if (value.compareTo(middle) < 0){
+            high = middle;
+            code = code + "0";
+        }
+        getLaCode(value,low,high,code);
+        return code;
+    }
+}
