826cf1bf3e05ab0ea33e96f5b14704a338a906da
CHH <949206605@qq.com>
2019-12-03 16:46:05 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/controller/TestController.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.honghao.cloud.userapi.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.honghao.cloud.userapi.base.BaseResponse;
 import com.honghao.cloud.userapi.common.enums.RoleTypeEnum;
 import com.honghao.cloud.userapi.dto.easypoi.WaybillBcListEasyPoi;
@@ -28,6 +29,7 @@ public class TestController {
 
     @GetMapping("/test001")
     public BaseResponse test001(@RequestParam @NotBlank String data){
+        JSONObject jsonObject = new JSONObject();
         List<Integer> list = Arrays.asList(1,3,5,8,9);
         list = list.stream().filter(each -> each < 4).collect(Collectors.toList());
         System.out.println(Arrays.toString(list.toArray()));
@@ -37,37 +39,43 @@ public class TestController {
         List<String> list1 = waybillBcLists.stream().filter(each ->
             Integer.valueOf(0).equals(each.getOrderType())
         ).map(WaybillBcListEasyPoi::getBatchId).collect(Collectors.toList());
-
+        jsonObject.put("list1",list1);
         List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
         numbers = numbers.stream()
                 .filter(i -> i % 2 == 0)
                 .distinct()
                 .skip(3)
                 .collect(Collectors.toList());
-
+        jsonObject.put("numbers",numbers);
         String[] s = {"hello","world"};
         List<String> list2 = Arrays.stream(s).
                 map(each -> each.split(""))
                 .flatMap(Arrays::stream)
                 .distinct().collect(Collectors.toList());
-
+        jsonObject.put("list2",list2);
         List<Integer> list3 = Arrays.asList(1, 2, 3, 4, 5);
         list3 = list3.stream().map(i -> i*i).collect(Collectors.toList());
 
         List<Integer> test01 = Arrays.asList(1,2,3);
         List<Integer> test02 = Arrays.asList(3,4);
         List<int[]> result = test01.stream().flatMap(i->test02.stream().map(j->new int[]{i,j})).collect(Collectors.toList());
-
+        jsonObject.put("result",result);
         List<int[]> result01 = test01.stream().flatMap(i->test02.stream().filter(j->(j+i)%3 == 0).map(j->new int[]{i,j})).collect(Collectors.toList());
-
+        jsonObject.put("result01",result01);
         Optional<WaybillBcListEasyPoi> dish = waybillBcLists.stream().filter(each ->"".equals(each.getBatchId())).findAny();
         if (dish.isPresent()){
             String wId = dish.get().getWId();
+            jsonObject.put("wId",wId);
         }else {
             System.out.println("结果不存在");
         }
         String name = RoleTypeEnum.formCode(1).getDesc();
-        return BaseResponse.successData(name);
+        jsonObject.put("name",name);
+        int sum = list3.stream().reduce(0, Integer::sum);
+        jsonObject.put("sum",sum);
+        int max = list3.stream().reduce(0, Integer::max);
+        jsonObject.put("max",max);
+        return BaseResponse.successData(jsonObject);
     }
 
 }
