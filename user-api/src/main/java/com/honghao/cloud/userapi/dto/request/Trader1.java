df5529ee036efefd9ec5146adef43dcf25563090
CHH <949206605@qq.com>
2019-12-13 19:25:10 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/dto/request/Trader1.java
@@ -0,0 +1,26 @@
+package com.honghao.cloud.userapi.dto.request;
+
+/**
+ * @author chenhonghao
+ * @date 2019-12-03 17:10
+ */
+public class Trader1 {
+    private String name;
+    private String city;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
