64b4fc7ed45497dc192863359d05129b7c098d55
CHH <949206605@qq.com>
2020-01-21 17:33:26 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/spring/Fox.java
@@ -0,0 +1,11 @@
+package com.honghao.cloud.userapi.spring;
+
+/**
+ * @author chenhonghao
+ * @date 2020-01-21 14:45
+ */
+public class Fox {
+    public Fox() {
+        System.out.println("========"+ Fox.class.getName());
+    }
+}
--- user-api/src/main/java/com/honghao/cloud/userapi/spring/TestMain.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/spring/TestMain.java
@@ -1,5 +1,8 @@
 package com.honghao.cloud.userapi.spring;
 
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.support.RootBeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
@@ -9,7 +12,20 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class TestMain {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext an = new AnnotationConfigApplicationContext(TestConfig.class);
-//        AppleA appleA = (AppleA) an.getBean("appleA");
-//        System.out.println(appleA);
+
+
+        BeanDefinition beanDefinition = new RootBeanDefinition(Fox.class);
+        //将fox交给spring容器进行管理   注册到beanDefinitionMap中，key = beanName  value = beanDefinition;
+//        an.registerBeanDefinition("fox",beanDefinition);
+//        an.getBean("fox");
+
+//        an.register(Fox.class);
+
+        //注册对象到容器中
+        Fox fox = new Fox();
+        DefaultListableBeanFactory defaultListableBeanFactory = an.getDefaultListableBeanFactory();
+        defaultListableBeanFactory.registerSingleton("fox",fox);
+
+        an.getBean("fox");
     }
 }
