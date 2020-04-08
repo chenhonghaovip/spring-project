21f40d1d857e480e8d1ef94b403c8f1c3475a96c
CHH <949206605@qq.com>
2020-04-08 17:51:16 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/LinkedHashMapTest.java
@@ -0,0 +1,81 @@
+package com.honghao.cloud.userapi.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author chenhonghao
+ * @date 2020-04-08 09:43
+ */
+public class LinkedHashMapTest<K,V> extends LinkedHashMap<K,V> {
+    private static final long serialVersionUID = 1L;
+
+    private final int maxCapacity;
+
+    private static final float DEFAULT_LOAD_FACTOR = 0.75f;
+
+    private final Lock lock = new ReentrantLock();
+
+    public LinkedHashMapTest(int maxCapacity) {
+        super(maxCapacity, DEFAULT_LOAD_FACTOR, true);
+        this.maxCapacity = maxCapacity;
+    }
+
+    @Override
+    public V get(Object key) {
+        lock.lock();
+        try {
+            return super.get(key);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public void clear() {
+        lock.lock();
+        try {
+            super.clear();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+        return size() > maxCapacity;
+    }
+
+    @Override
+    public int size() {
+        lock.lock();
+        try {
+            return super.size();
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    @Override
+    public V put(K key, V value) {
+        lock.lock();
+        try {
+            return super.put(key, value);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public Collection<Map.Entry<K, V>> getAll() {
+        lock.lock();
+        try {
+            return new ArrayList<>(super.entrySet());
+        } finally {
+            lock.unlock();
+        }
+    }
+}
