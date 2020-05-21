01f9cad47415c4d0abf79e7d667ab30f0edfb6a2
chenhonghao <949206605@qq.com>
2020-05-21 23:08:23 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/face/Test11.java
@@ -0,0 +1,37 @@
+package com.honghao.cloud.userapi.test.face;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author chenhonghao
+ * @date 2020-05-21 20:02
+ */
+public class Test11 {
+    private final static Object lock = new Object();
+    static AtomicInteger atomicInteger = new AtomicInteger(1);
+    private static ReentrantLock reentrantLock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        new Thread(() -> test01(),"1").start();
+
+        new Thread(() -> test01(),"2").start();
+
+        new Thread(() -> test01(),"0").start();
+
+    }
+
+    private static void test01(){
+        while (atomicInteger.get()<=99){
+            reentrantLock.lock();
+            try {
+                if (atomicInteger.get()<=99 && atomicInteger.get()%3==Integer.valueOf(Thread.currentThread().getName())){
+                    System.out.println(Thread.currentThread().getName()+"===="+atomicInteger.getAndIncrement());
+                }
+            } finally {
+                reentrantLock.unlock();
+            }
+        }
+
+    }
+}
