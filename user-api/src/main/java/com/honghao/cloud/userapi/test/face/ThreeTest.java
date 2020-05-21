10363d2a27c0615015e7995efaac45bad5cfebde
chenhonghao <949206605@qq.com>
2020-05-21 23:08:23 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/face/ThreeTest.java
@@ -0,0 +1,15 @@
+package com.honghao.cloud.userapi.test.face;
+
+import org.junit.Test;
+
+/**
+ * @author chenhonghao
+ * @date 2020-05-21 22:13
+ */
+public class ThreeTest {
+
+    @Test
+    public void test(){
+
+    }
+}
