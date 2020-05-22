09a98294c151e5d2901f96664ae6c9c693a28c58
CHH <949206605@qq.com>
2020-05-22 16:29:48 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/face/ConditionTest.java
@@ -0,0 +1,80 @@
+package com.honghao.cloud.userapi.test.face;
+
+import com.honghao.cloud.userapi.factory.ExecutorFactory;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 使用Condition接口实现线程协同
+ *
+ * @author chenhonghao
+ * @date 2020-05-22 14:16
+ */
+public class ConditionTest {
+    private static ReentrantLock reentrantLock = new ReentrantLock();
+
+    public static void main(String[] args) {
+        Condition condition = reentrantLock.newCondition();
+        AtomicInteger atomicInteger = new AtomicInteger(0);
+        ThreadPoolExecutor threadPoolExecutor = ExecutorFactory.buildThreadPoolExecutor(3, 3, "test");
+        threadPoolExecutor.submit(()-> {
+            while (true){
+                reentrantLock.lock();
+                try {
+                    while (atomicInteger.get()%3!=0){
+                        condition.await();
+                    }
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"==="+atomicInteger.getAndIncrement());
+                        condition.signalAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantLock.unlock();
+                }
+            }
+        });
+
+        threadPoolExecutor.submit(()-> {
+            while (true){
+                reentrantLock.lock();
+                try {
+                    while (atomicInteger.get()%3!=1){
+                        condition.await();
+                    }
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"==="+atomicInteger.getAndIncrement());
+                        condition.signalAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantLock.unlock();
+                }
+            }
+        });
+
+        threadPoolExecutor.submit(()-> {
+            while (true){
+                reentrantLock.lock();
+                try {
+                    while (atomicInteger.get()%3!=2){
+                        condition.await();
+                    }
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"==="+atomicInteger.getAndIncrement());
+                        condition.signalAll();
+                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    reentrantLock.unlock();
+                }
+            }
+        });
+    }
+}
---
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/face/ThreadCoordinationTest.java
@@ -0,0 +1,80 @@
+package com.honghao.cloud.userapi.test.face;
+
+import com.honghao.cloud.userapi.factory.ExecutorFactory;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 通过使用wait和notify实现线程协同
+ * @author chenhonghao
+ * @date 2020-05-22 09:15
+ */
+public class ThreadCoordinationTest {
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+    private final Object object = new Object();
+
+    public static void main(String[] args) {
+        ThreadCoordinationTest threadCoordinationTest = new ThreadCoordinationTest();
+        threadCoordinationTest.test();
+    }
+    public void test() {
+
+        ThreadPoolExecutor threadPoolExecutor = ExecutorFactory.buildThreadPoolExecutor(3, 3, "test");
+
+        threadPoolExecutor.submit(()-> {
+            while (true){
+                synchronized (object){
+                    while (atomicInteger.get()%3!=0){
+                        try {
+                            object.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"=="+atomicInteger.getAndIncrement());
+                        object.notifyAll();
+                    }
+                }
+            }
+        });
+
+        threadPoolExecutor.submit(()-> {
+            while (true){
+                synchronized (object){
+                    while (atomicInteger.get()%3!=1){
+                        try {
+                            object.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"=="+atomicInteger.getAndIncrement());
+                        object.notifyAll();
+                    }
+                }
+            }
+        });
+
+        threadPoolExecutor.submit(()-> {
+            while (true){
+                synchronized (object){
+                    while (atomicInteger.get()%3!=2){
+                        try {
+                            object.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"=="+atomicInteger.getAndIncrement());
+                        object.notifyAll();
+                    }
+                }
+            }
+        });
+
+    }
+}
