8e431c6d15f3556c7deee18a114e94edcfe151e6
CHH <949206605@qq.com>
2020-05-22 16:29:48 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/test/face/ThreeTest.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/face/ThreeTest.java
@@ -1,5 +1,9 @@
 package com.honghao.cloud.userapi.test.face;
 
+import com.honghao.cloud.userapi.factory.ExecutorFactory;
+
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -9,71 +13,71 @@ import java.util.concurrent.locks.ReentrantLock;
  * @date 2020-05-21 22:13
  */
 public class ThreeTest {
-    private static int count = 0;
     private static Lock lock = new ReentrantLock();
-
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
     public static void main(String[] args) {
         Condition f1 = lock.newCondition();
         Condition f2 = lock.newCondition();
         Condition f3 = lock.newCondition();
 
-        new Thread(()->{
-            while(true) {
+        ThreadPoolExecutor threadPoolExecutor = ExecutorFactory.buildThreadPoolExecutor(3, 3, "test");
+        threadPoolExecutor.submit(()->{
+            while(atomicInteger.get()<=99) {
                 lock.lock();
                 try {
-                    while(count %3 != 0) {
-                        //刚开始count为0  0%3=0 所以此线程执行  执行完之后 唤醒现成2，由于此时count已经进行了++，所有while成立，c1进入等待状态，其他两个也一样
+                    while(atomicInteger.get() %3 != 0) {
+                        // 释放lock锁，当前线程进入等待状态,进入到f1的等待队列中去
                         f1.await();
                     }
-                    System.out.println(Thread.currentThread().getName()+"========:A");
-                    count++;
-                    //唤醒线程2
-                    f2.signal();
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"========:"+atomicInteger.getAndIncrement());
+                        // 唤醒线程2
+                        f2.signal();
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
                     lock.unlock();
                 }
             }
-        }) .start();
-
-        new Thread(()->{
-            while(true) {
+        });
+        threadPoolExecutor.submit(()->{
+            while(atomicInteger.get()<=99) {
                 lock.lock();
                 try {
-                    while(count %3 != 1) {
+                    while(atomicInteger.get() %3 != 1) {
                         f2.await();
                     }
-                    System.out.println(Thread.currentThread().getName()+"========:B");
-                    count++;
-                    //唤醒线程3
-                    f3.signal();
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"========:"+atomicInteger.getAndIncrement());
+                        //唤醒线程3
+                        f3.signal();
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
                     lock.unlock();
                 }
             }
-        }) .start();
-
-        new Thread(()->{
-            while(true) {
+        });
+        threadPoolExecutor.submit(()->{
+            while(atomicInteger.get()<=99) {
                 lock.lock();
                 try {
-                    while(count %3 != 2) {
+                    while(atomicInteger.get() %3 != 2) {
                         f3.await();
                     }
-                    System.out.println(Thread.currentThread().getName()+"========:C");
-                    count++;
-                    //唤醒线程1
-                    f1.signal();
+                    if (atomicInteger.get()<=99){
+                        System.out.println(Thread.currentThread().getName()+"========:"+atomicInteger.getAndIncrement());
+                        //唤醒线程1
+                        f1.signal();
+                    }
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 } finally {
                     lock.unlock();
                 }
             }
-        }) .start();
-
+        });
     }
 }
