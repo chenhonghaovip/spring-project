81077968611f9219b71cae1274af4a134168c137
CHH <949206605@qq.com>
2020-06-24 09:39:42 +0800

    commit by chh

---
+++ user-api/src/main/java/com/honghao/cloud/userapi/test/face/thread/ThreadCoordinationTest1.java
@@ -0,0 +1,63 @@
+package com.honghao.cloud.userapi.test.face.thread;
+
+import com.honghao.cloud.userapi.factory.ExecutorFactory;
+
+import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 通过使用wait和notify实现线程协同
+ * @author chenhonghao
+ * @date 2020-05-22 09:15
+ */
+public class ThreadCoordinationTest1 {
+    private static AtomicInteger atomicInteger = new AtomicInteger(0);
+    private final Object object = new Object();
+
+    public static void main(String[] args) {
+        ThreadCoordinationTest1 threadCoordinationTest = new ThreadCoordinationTest1();
+        threadCoordinationTest.test();
+    }
+    public void test() {
+
+        ThreadPoolExecutor threadPoolExecutor = ExecutorFactory.buildThreadPoolExecutor(3, 3, "test");
+        CyclicBarrier cyclicBarrier = new CyclicBarrier(3);
+        threadPoolExecutor.submit(()-> {
+            int a ;
+            while ((a = atomicInteger.get())<=99){
+                if (a%3==0){
+                    synchronized (object){
+                        System.out.println(Thread.currentThread().getName()+"=="+atomicInteger.getAndIncrement());
+                        object.notifyAll();
+                    }
+                }
+            }
+        });
+
+        threadPoolExecutor.submit(()-> {
+            int a ;
+            while ((a = atomicInteger.get())<=99){
+                if (a%3==1){
+                    synchronized (object){
+                        System.out.println(Thread.currentThread().getName()+"=="+atomicInteger.getAndIncrement());
+                        object.notifyAll();
+                    }
+                }
+            }
+        });
+
+        threadPoolExecutor.submit(()-> {
+            int a ;
+            while ((a = atomicInteger.get())<=99){
+                if (a%3==2){
+                    synchronized (object){
+                        System.out.println(Thread.currentThread().getName()+"=="+atomicInteger.getAndIncrement());
+                        object.notifyAll();
+                    }
+                }
+            }
+        });
+
+    }
+}
