0fc92bcbf9900dd097bdfa86341d19dbada77ee1
CHH <949206605@qq.com>
2020-07-22 18:09:07 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/facade/impl/BatchFacadeImpl.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/facade/impl/BatchFacadeImpl.java
@@ -6,7 +6,6 @@ import com.honghao.cloud.userapi.domain.entity.WaybillBcList;
 import com.honghao.cloud.userapi.facade.BatchFacade;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.Test;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +15,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.LockSupport;
 import java.util.stream.Collectors;
 
 /**
@@ -25,9 +26,13 @@ import java.util.stream.Collectors;
 @Slf4j
 @Service
 public class BatchFacadeImpl implements BatchFacade {
+    private static final AtomicInteger ATOMIC_INTEGER = new AtomicInteger(0);
     private static final int max = 1000;
 
     private LinkedBlockingQueue<Request> queues = new LinkedBlockingQueue<>();
+
+    private LinkedBlockingQueue<Request> queues1 = new LinkedBlockingQueue<>();
+
 
     @Resource
     private OrderClient orderClient;
@@ -36,12 +41,15 @@ public class BatchFacadeImpl implements BatchFacade {
         private String movieCode;
 
         private CompletableFuture<WaybillBcList> future;
+
+        private Thread thread;
+
+        private WaybillBcList waybillBcList;
     }
 
     @PostConstruct
     public void init(){
-        ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1);
-//        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+        ScheduledExecutorService scheduledExecutorService = new ScheduledThreadPoolExecutor(1, t-> new Thread(""+ATOMIC_INTEGER.getAndIncrement()));
         scheduledExecutorService.scheduleAtFixedRate(() -> {
             List<Request> list = new ArrayList<>();
             if (queues.size()==0) {
@@ -61,8 +69,26 @@ public class BatchFacadeImpl implements BatchFacade {
             Map<String,WaybillBcList> resultMap = new HashMap<>(responses.size()*2);
             responses.forEach(each->resultMap.put(each.getWId(),each));
 
+
             for (Request request : list) {
                 request.getFuture().complete(resultMap.get(request.getMovieCode()));
+            }
+
+            if (queues1.size()==0){
+                return;
+            }
+            List<Request> list1 = new ArrayList<>();
+            for (int i = 0; i < queues1.size(); i++) {
+                list1.add(queues1.poll());
+            }
+            List<String> data1 = list1.stream().map(Request::getMovieCode).collect(Collectors.toList());
+            //批量请求
+            List<WaybillBcList> responses1 = orderClient.batchQuery(data1);
+            Map<String,WaybillBcList> resultMap1 = new HashMap<>(responses.size()*2);
+            responses1.forEach(each -> resultMap1.put(each.getWId(), each));
+            for (Request request : list1) {
+                request.setWaybillBcList(resultMap1.get(request.getMovieCode()));
+                LockSupport.unpark(request.getThread());
             }
 
         },0,10, TimeUnit.MILLISECONDS);
@@ -79,18 +105,14 @@ public class BatchFacadeImpl implements BatchFacade {
         return BaseResponse.successData(future.get());
     }
 
-
-    @Test
-    public void getIn(){
-        List<String> list = new ArrayList<>();
-        list.add("11");
-        list.add("22");
-        list.remove("11");
-
-
-        Map<String,String> map = new HashMap<>();
-        map.put("1","1");
-        map.remove("1");
-        System.out.println(map);
+    public BaseResponse<WaybillBcList> queryCommon1(String data) {
+        Request request = new Request();
+        request.setMovieCode(data);
+        request.setThread(Thread.currentThread());
+        queues1.add(request);
+        LockSupport.park();
+        return BaseResponse.successData(request.getWaybillBcList());
     }
+
+
 }
