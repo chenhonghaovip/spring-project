ee981a6a3c2209bd1bf2b04ceb56747ef4662c1a
CHH <949206605@qq.com>
2020-07-27 10:16:03 +0800

    commit by chh

--- tx-manage-starter/src/test/java/com/honghao/cloud/txmanagestarter/TxManageStarterApplicationTests.java
+++ tx-manage-starter/src/test/java/com/honghao/cloud/txmanagestarter/TxManageStarterApplicationTests.java
@@ -1,13 +1,10 @@
 package com.honghao.cloud.txmanagestarter;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TxManageStarterApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+
 
 }
