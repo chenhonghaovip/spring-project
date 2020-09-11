c2d0f0188333ca2f573868aad346a35618a39d45
CHH <949206605@qq.com>
2020-09-11 17:34:59 +0800

    commit by chh

--- user-api/src/main/java/com/honghao/cloud/userapi/config/Swagger2.java
+++ user-api/src/main/java/com/honghao/cloud/userapi/config/Swagger2.java
@@ -1,4 +1,4 @@
-package com.honghao.cloud.userapi;
+package com.honghao.cloud.userapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
